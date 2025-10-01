e8043119f61b580dd2809a8bf78e6bbd17f9bf30
Vladislav Slepov <ikigai@ikigai-dev.ru>
2025-10-02 00:22:44 +0300

    Remove unnecessary conan repos

--- conanfile.py
+++ conanfile.py
@@ -17,8 +17,8 @@ class StormEngine(ConanFile):
     # dependencies used in deploy binaries
     # conan-center
     requires = ["zlib/1.2.11", "spdlog/1.9.2", "fast_float/3.4.0", "sdl/2.0.18", "mimalloc/2.0.3", "sentry-native/0.5.0",
-    # storm.jfrog.io
-    "directx/9.0@storm/prebuilt", "fmod/2.02.05@storm/prebuilt"]
+    # exported packages
+    "directx/9.0@ikigai/stable", "fmod/2.02.05@ikigai/stable"]
     # aux dependencies (e.g. for tests)
     build_requires = "catch2/2.13.7"
 
